7f689faa1976cce50a638b51f47a7a0e1e648d58
solomanhl <61859250@qq.com>
2016-02-18 17:21:18 +0800

    完成遮罩拍裤子，合成照片

---
+++ app/src/main/java/com/solomanhl/mycloset/maskImage/ImageAddMask_kuzi.java
@@ -0,0 +1,21 @@
+package com.solomanhl.mycloset.maskImage;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.solomanhl.mycloset.R;
+
+public class ImageAddMask_kuzi extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.imageaddmask_kuzi);
+
+        findView();
+    }
+
+    private void findView() {
+        // TODO Auto-generated method stub
+    }
+}
